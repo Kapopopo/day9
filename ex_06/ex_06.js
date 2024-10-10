b7e0dc7c4ed18f157dfa45a0b799066b699d6c2c
kapopay <nawfal.yessoufou@epitech.eu>
2024-10-10 21:51:18 +0200

    finish

---
+++ ex_06/ex_06.js
@@ -0,0 +1,35 @@
+particlesJS("particules_top1", {
+  particles: {
+    number: { value: 100 },
+    color: { value: "#ff0000" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+  },
+});
+
+particlesJS("particules_top2", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#00ff00" },
+    shape: { type: "edge" },
+    opacity: { value: 0.3 },
+  },
+});
+
+particlesJS("particules_bottom1", {
+  particles: {
+    number: { value: 120 },
+    color: { value: "#0000ff" },
+    shape: { type: "triangle" },
+    opacity: { value: 0.7 },
+  },
+});
+
+particlesJS("particules_bottom2", {
+  particles: {
+    number: { value: 150 },
+    color: { value: "#ff00ff" },
+    shape: { type: "polygon" },
+    opacity: { value: 0.9 },
+  },
+});
